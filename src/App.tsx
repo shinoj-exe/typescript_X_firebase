e4935d6a74428904ce6b3ee357580b60319ffe4a
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2023-03-22 23:07:49 +0530

    project steup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router ,Route,Routes} from 'react-router-dom'
+import Main from './pages/main';
 import './App.css';
+import Login from './pages/login';
+import Navbar from './components/navbar';
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Main/>}/>
+          <Route path="/login" element={<Login/>}/>
+          {/* <Route path="/" element={<Main/>}/> */}
 
+        </Routes>
+      </Router>
     </div>
   );
 }
