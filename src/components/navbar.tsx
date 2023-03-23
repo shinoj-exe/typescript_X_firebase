12e8a8f31f2306f2818e608134a1ca1b0ab83303
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2023-03-23 12:22:51 +0530

    google authentication

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { auth,provider } from '../config/firebase'
+import {useAuthState} from "react-firebase-hooks/auth"
+import {signOut} from 'firebase/auth'
+
 const Navbar = () => {
+  // const [user,loading,error] = useAuthState(auth)
+  const [user] = useAuthState(auth)
+
+  const signUserOut=async()=>{
+    await signOut(auth)
+  }
+
   return (
     <div>
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
+
+          <div className='user'>
+            {
+              user && 
+              <div>
+                <p>{user?.displayName}</p>
+                <img src={user?.photoURL || ""} width="100px" height="100px" />
+                <button onClick={signUserOut}>Log Out</button>
+              </div>
+            }
+        </div>
     </div>
   )
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { auth,provider } from '../config/firebase'
+import {signInWithPopup} from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const navigate = useNavigate();
+  const signInWithGoogle = async ()=>{
+    const result = await signInWithPopup(auth,provider)
+    console.log(result);
+    navigate("/")
+  }
   return (
-    <div>Login</div>
+    <div>
+      <p>Sign In With Google</p>
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    </div>
   )
 }
 
